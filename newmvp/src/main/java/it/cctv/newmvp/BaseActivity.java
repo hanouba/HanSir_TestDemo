34e6fc090def12e0819a528d2fbe18e2e021c84a
HanSir <1356548475@qq.com>
2017-08-25 16:48:48 +0800

    测试提交

--- newmvp/src/main/java/it/cctv/newmvp/BaseActivity.java
+++ newmvp/src/main/java/it/cctv/newmvp/BaseActivity.java
@@ -3,8 +3,11 @@ package it.cctv.newmvp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import javax.inject.Inject;
+
+import dagger.Component;
 
 /**
  * 创建者 by ${HanSir} on 2017/8/25.
@@ -23,7 +26,7 @@ public class BaseActivity extends AppCompatActivity implements IView {
 
     @Override
     public void success(Object o) {
-
+//        Log.e(TAG, "success: dd", );
     }
 
     @Override
